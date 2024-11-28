4abb00c4eb4df011fba2ee9a630206d62e6f27f2
admin <tany--96@mail.ru>
2024-11-28 19:57:08 +0300

    push me again

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/i-music',
+  // output: 'export',
+  // basePath: '/i-music',
   async redirects() {
     return [
       {
--- src/app/trackPages/selection/[id]/page.tsx
+++ src/app/trackPages/selection/[id]/page.tsx
@@ -5,10 +5,19 @@ import { useEffect } from 'react';
 import Select from '@/components/Select/Select';
 import { useAppDispatch } from '@/store/store';
 import { addSelectionTracks } from '@/store/features/trackSlice';
+// import {getSelectionTracks} from "@/api/selectionApi";
 
 type IdType = {
   id: string;
 };
+
+// export async function generateStaticParams() {
+//   const tracks = await getSelectionTracks('1')
+//
+//   return tracks.items.map((track) => ({
+//     id: track._id,
+//   }))
+// }
 
 function Selection() {
   const dispatch = useAppDispatch();
